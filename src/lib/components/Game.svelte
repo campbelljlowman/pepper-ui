5054f5edfd203d7ad72dcf6c3f51fc383f38174f
Campbell Lowman <campbelllowman@gmail.com>
2023-10-04 16:35:21 -0400

    display games with logos

--- src/lib/components/Game.svelte
+++ src/lib/components/Game.svelte
@@ -4,15 +4,24 @@
      export let away_team_name: string;
      export let away_team_logo: string;
      export let timeimestamptz: Date;
+     export let stream_link: string | null;
 </script>
 
 <div class="flex justify-around items-center border-4 rounded p-4 m-4">
-    <div class="font-semibold w-1/2">
-        <p>{home_team_name}</p>
-        <p>{away_team_name}</p>
+    <div class="font-semibold w-2/3">
+        <div class="flex gap-1">
+            <img class="h-6 w-6" src={home_team_logo} alt="Home team logo"/>
+            <p>{home_team_name}</p>
+        </div>
+        <div class="flex gap-1 items-center">
+            <img class="h-6 w-6" src={away_team_logo} alt="Away team logo"/>
+            <p>{away_team_name}</p>
+        </div>
     </div>
-    <div class="flex flex-col justify-around items-center w-1/2">
-        <p class="text-sm">{timeimestamptz}</p>
-        <a href='/watch/asdf' class="hover:bg-[var(--background-hover)] rounded">Watch</a>
+    <div class="flex flex-col justify-around items-center w-1/3">
+        <p class="text-sm">{timeimestamptz.toLocaleTimeString([], {hour: 'numeric', minute:'2-digit'})}</p>
+        {#if stream_link !== null && stream_link !== ''}
+            <a href='/watch/{stream_link}' class="hover:bg-[var(--background-hover)] rounded">Watch</a>
+        {/if}
     </div>
 </div>
--- src/routes/MLB/+page.svelte
+++ src/routes/MLB/+page.svelte
@@ -5,15 +5,17 @@
     console.log(data);
 </script>
 
-{#each data.mlb_games_today as mlb_game}
-    {#if mlb_game.home_team !== null && mlb_game.away_team !== null}
-        <Game 
-            home_team_name={mlb_game.home_team.display_name}
-            home_team_logo={mlb_game.home_team.logo}
-            away_team_name={mlb_game.away_team.display_name}
-            away_team_logo={mlb_game.away_team.logo}
-            timeimestamptz={new Date(mlb_game.timestamptz)}
-        />
-    {/if}
-{/each}
-<h1>MLB</h1>
+<div class="grid grid-cols-3 p-4">
+    {#each data.mlb_games_today as mlb_game}
+        {#if mlb_game.home_team !== null && mlb_game.away_team !== null}
+            <Game 
+                home_team_name={mlb_game.home_team.display_name}
+                home_team_logo={mlb_game.home_team.logo}
+                away_team_name={mlb_game.away_team.display_name}
+                away_team_logo={mlb_game.away_team.logo}
+                timeimestamptz={new Date(mlb_game.timestamptz)}
+                stream_link={mlb_game.stream_link}
+            />
+        {/if}
+    {/each}
+</div>
